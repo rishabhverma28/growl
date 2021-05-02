69946788e806a64486142659e80d7d3ee8aea498
Rishabh Verma <rishabhverma.2806@gmail.com>
2021-05-02 22:10:24 +1000

    modify according to task description

--- src/App.js
+++ src/App.js
@@ -6,26 +6,31 @@ import './App.css'
 import { useGrowl, Growl } from './growl'
 
 function App() {
-	const [active, setActive] = useGrowl()
+  const [active, setActive, setTimeInMS] = useGrowl()
+  setTimeInMS()
 
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<Link className="App-link" to="/other">
-					Go to the Other component
-        		</Link>
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-        		</p>
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Link className="App-link" to="/other">
+          Go to the Other component
+        </Link>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
 
-				<a className="App-link" href="#" onClick={() => void setActive(true)}>
-					Clik here to activate the growl
-        		</a>
-			</header>
-			<Growl onDismissed={() => setActive(false)} active={active} message="Hello World!" />
-		</div>
-	)
+        <a className="App-link" href="/#" onClick={() => void setActive(true)}>
+          Click here to activate the growl
+        </a>
+      </header>
+      <Growl
+        onDismissed={() => setActive(false)}
+        active={active}
+        message="Hello World!"
+      />
+    </div>
+  )
 }
 
-export default App;
+export default App
--- src/growl.js
+++ src/growl.js
@@ -1,25 +1,34 @@
 import React from 'react'
 
 import './growl.css'
+const DEFAULT_TIME_FOR_ACTIVE_GROWL_MS = 3000
 
 export const Growl = ({ active, message, onDismissed }) => (
-  <div className={`growl${active ? " active" : ""}`}>
+  <div className={`growl${active ? ' active' : ''}`}>
     {message}
     <div onClick={onDismissed} className="growl-close" />
   </div>
 )
 
 export function useGrowl() {
-    // state of the growl
-    const [growlActive, setGrowlActive] = React.useState(false)
+  // state of the growl
+  const [growlActive, setGrowlActive] = React.useState(false)
+  const setTimeInMS = (timeInMS) => {
+    setTimeout(() => {
+      setGrowlActive(false)
+    }, timeInMS)
+  }
 
-    return [
-        // the first arg is the state
-        growlActive, 
+  return [
+    // the first arg is the state
+    growlActive,
 
-        // the second arg is a fn that allows you to safely set its state
-        (active) => {
-            setGrowlActive(active)
-        },
-    ]
+    // the second arg is a fn that allows you to safely set its state
+    (active) => {
+      setGrowlActive(active)
+    },
+    (timeInMS = DEFAULT_TIME_FOR_ACTIVE_GROWL_MS) => {
+      setTimeInMS(timeInMS)
+    },
+  ]
 }
